c0cc60b035b8d9056ec2814d453e1ca744cb718d
Shamil-G <s.gusseynov@gmail.com>
2023-06-12 17:15:42 +0600

    Change start.bat

--- start.bat
+++ start.bat
@@ -4,4 +4,6 @@ rem call %VIRTUAL_ENV%/bin/activate
 call %VIRTUAL_ENV%/Scripts/activate.bat
 pip3.10 install --upgrade pip
 rem pip3.10 install flask
+pip3.10 uninstall cx_Oracle
+pip3.10 install oracledb
 python main_app.py
