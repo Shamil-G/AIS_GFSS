3167422f0bed028437f48aa0c894e329dd6095fe
Гусейнов_Ш <s.gusseynov@gmail.com>
2023-09-08 12:53:20 +0600

    Добавлен отчет ЕП rep_dia_cp_02, сделаны другие корректировки

--- start.bat
+++ start.bat
@@ -1,7 +1,7 @@
-set VIRTUAL_ENV=C:/Projects/AIS_GFSS/env
-rem python3.10 -m venv venv
+set VIRTUAL_ENV=C:/Projects/AIS_GFSS/venv
+rem python -m venv venv
 rem call %VIRTUAL_ENV%/bin/activate
 call %VIRTUAL_ENV%/Scripts/activate.bat
-pip3.10 install --upgrade pip
-rem pip3.10 install flask
-python main_app.py
+python -m pip install --upgrade pip
+rem pip install flask
+rem python main_app.py
