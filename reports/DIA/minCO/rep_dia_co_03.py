c734da5487696c09ef89b96473926cf48244fa26
Гусейнов_Ш <s.gusseynov@gmail.com>
2024-12-02 09:24:04 +0500

    ADD report rep_dia_co_03 ADD function get_quarter_number

---
+++ reports/DIA/minCO/rep_dia_co_03.py
@@ -0,0 +1,279 @@
+from configparser import ConfigParser
+import xlsxwriter
+import datetime
+from   util.logger import log
+import oracledb
+import os.path
+from   model.manage_reports import set_status_report
+from   util.trunc_date import get_quarter_number
+
+report_name = 'Уплаченные СО в размере менее 1 МЗП за квартал. Для проведения информационно-разъяснительной работы'
+report_code = 'minCO.03'
+
+stmt_report = """
+with 
+dm as(
+  select to_date('01.'||lpad((n_q-1)*3,2,'0')||'.'||n_year,'dd.mm.yyyy') as f_month,
+         to_date('01.'||lpad((n_q-1)*3+1,2,'0')||'.'||n_year,'dd.mm.yyyy') as s_month,
+         to_date('01.'||lpad((n_q-1)*3+2,2,'0')||'.'||n_year,'dd.mm.yyyy') as t_month,
+         n_year
+  from (
+        select
+               ceil(extract(month from to_date(:first_date,'yyyy-mm-dd'))/3) n_q,
+               extract(year from to_date(:first_date,'yyyy-mm-dd')) n_year
+        from dual
+       )
+)
+, aq as 
+(
+    SELECT /*+PARALLEL (4)*/ 
+           trunc(pay_date, 'MM') pay_mnth, 
+           sicid, 
+           p_rnn, 
+           pay_month, 
+           SUM(cnt_mzp) sum_mzp, 
+           SUM(sum_pay) sum_pay 
+    FROM si_member_2, dm
+    WHERE pay_date >= dm.f_month
+    AND pay_date < add_months(dm.f_month,3) 
+    AND pay_month < add_months(dm.f_month,3)
+    AND pay_month >= to_date('2013-02-01','yyyy-mm-dd')  -- 1 МЗП с февраля 2013 года
+    AND knp in ('012')
+    AND p_rnn !='160440007161'
+    GROUP BY trunc(pay_date, 'MM'), sicid, p_rnn, pay_month
+    HAVING SUM(cnt_mzp) < 1
+) 
+SELECT
+      m7.obc,
+      m7.obn,
+      m7.rc,
+      m7.rn,
+      m7.p_rnn,
+	  m7.nm,
+      m7.c,
+      m8.c,
+      m9.c
+FROM 
+  ( 
+  SELECT
+      p_rnn,
+      rb1.rfbn_id obc,
+      rb1.name obn,
+      rb.rfbn_id rc,
+      rb.name rn,
+      nvl(n.name_ip, n.fio) nm,
+      COUNT(DISTINCT sicid) c
+  FROM aq m
+  LEFT JOIN rfrr_id_region r ON r.id = m.p_rnn AND r.typ = 'I'
+  LEFT JOIN nk_minfin_iin n ON n.iin = m.p_rnn 
+  LEFT JOIN rfbn_branch_site rb ON rb.RFBN_ID = r.rfbn_id
+  LEFT JOIN rfbn_branch_site rb1 ON rb1.RFBN_ID = r.rfrg_id || '00'
+  , dm
+  where   m.sum_mzp < 1
+  AND   m.pay_month = dm.f_month
+  GROUP BY p_rnn
+        , nvl(n.name_ip, n.fio), rb1.rfbn_id, rb1.name, rb.rfbn_id, rb.name
+  ) m7,
+  ( SELECT p_rnn, COUNT(DISTINCT sicid) c
+    FROM aq m, dm
+    WHERE sum_mzp < 1
+    AND m.pay_month = dm.s_month
+    GROUP BY p_rnn
+  ) m8,
+  ( SELECT p_rnn, COUNT(DISTINCT sicid) c
+    FROM aq m, dm
+    WHERE sum_mzp < 1
+    AND m.pay_month = dm.t_month
+    GROUP BY p_rnn
+  ) m9
+WHERE m7.p_rnn = m8.p_rnn 
+AND m8.p_rnn = m9.p_rnn
+ORDER BY 1, 3
+"""
+
+def format_worksheet(worksheet, common_format):
+	worksheet.set_row(0, 24)
+	worksheet.set_row(1, 24)
+	worksheet.set_row(2, 14)
+	#worksheet.set_row(3, 48)
+
+	worksheet.set_column(0, 0, 6)
+	worksheet.set_column(1, 1, 11)
+	worksheet.set_column(2, 2, 42)
+	worksheet.set_column(3, 3, 11)
+	worksheet.set_column(4, 4, 48)
+	worksheet.set_column(5, 5, 16)
+	worksheet.set_column(6, 6, 128)
+	worksheet.set_column(7, 7, 8)
+	worksheet.set_column(8, 8, 8)
+	worksheet.set_column(9, 9, 8)
+
+	worksheet.write(2,0, '1', common_format)
+	worksheet.write(2,1, '2', common_format)
+	worksheet.write(2,2, '3', common_format)
+	worksheet.write(2,3, '4', common_format)
+	worksheet.write(2,4, '5', common_format)
+	worksheet.write(2,5, '6', common_format)
+	worksheet.write(2,6, '7', common_format)
+	worksheet.write(2,7, '8', common_format)
+	worksheet.write(2,8, '9', common_format)
+	worksheet.write(2,9, '10', common_format)
+
+	worksheet.write(3,0, '№', common_format)
+	worksheet.write(3,1, 'Код области', common_format)
+	worksheet.write(3,2, 'Область', common_format)
+	worksheet.write(3,3, 'Код района', common_format)
+	worksheet.write(3,4, 'Район', common_format)
+	worksheet.write(3,5, 'БИН предприятия', common_format)
+	worksheet.write(3,6, 'Наименование предприятия', common_format)
+	worksheet.write(3,7, '1 месяц', common_format)
+	worksheet.write(3,8, '2 месяц', common_format)
+	worksheet.write(3,9, '3 месяц', common_format)
+	
+
+def do_report(file_name: str, date_first: str):
+	if os.path.isfile(file_name):
+		log.info(f'Отчет уже существует {file_name}: {date_first}')
+		return file_name
+	log.info(f'DO REPORT. START {report_code}. DATE_FROM: {date_first}, FILE_PATH: {file_name}')
+
+	config = ConfigParser()
+	config.read('db_config.ini')
+	
+	ora_config = config['rep_db_12']
+	db_user=ora_config['db_user']
+	db_password=ora_config['db_password']
+	db_dsn=ora_config['db_dsn']
+	log.info(f'{report_code}. db_user: {db_user}, db_dsn: {db_dsn}')
+	
+	with oracledb.connect(user=db_user, password=db_password, dsn=db_dsn) as connection:
+		with connection.cursor() as cursor:
+			workbook = xlsxwriter.Workbook(file_name)
+
+			title_format = workbook.add_format({'bg_color': '#D1FFFF', 'align': 'center', 'font_color': 'black'})
+			#title_format = workbook.add_format({'bg_color': '#C5FFFF', 'align': 'center', 'font_color': 'black'})
+			title_format.set_align('vcenter')
+			title_format.set_border(1)
+			title_format.set_text_wrap()
+			title_format.set_bold()
+
+			title_name_report = workbook.add_format({'align': 'left', 'font_color': 'black', 'font_size': '14'})
+			title_name_report .set_align('vcenter')
+			title_name_report .set_bold()
+
+			common_format = workbook.add_format({'align': 'center', 'font_color': 'black'})
+			common_format.set_align('vcenter')
+			common_format.set_border(1)
+
+			name_format = workbook.add_format({'align': 'left', 'font_color': 'black'})
+			name_format.set_align('vcenter')
+			name_format.set_border(1)
+
+			sum_pay_format = workbook.add_format({'num_format': '#,###,##0.00', 'font_color': 'black', 'align': 'vcenter'})
+			sum_pay_format.set_border(1)
+
+			date_format = workbook.add_format({'num_format': 'dd.mm.yyyy', 'align': 'center'})
+			date_format.set_border(1)
+			date_format.set_align('vcenter')
+
+			date_format_italic = workbook.add_format({'num_format': 'dd.mm.yyyy', 'align': 'center'})
+			date_format_italic.set_italic()
+			#date_format_italic.set_border(0)
+
+			digital_format = workbook.add_format({'num_format': '#0', 'align': 'center'})
+			digital_format.set_border(1)
+			digital_format.set_align('vcenter')
+
+			money_format = workbook.add_format({'num_format': '# ### ### ##0', 'align': 'right'})
+			money_format.set_border(1)
+			money_format.set_align('vcenter')
+
+			now = datetime.datetime.now()
+			start_time = now.strftime("%H:%M:%S")
+			log.info(f'Начало формирования {file_name}: {now.strftime("%d-%m-%Y %H:%M:%S")}')
+			worksheet = workbook.add_worksheet('Список')
+			sql_sheet = workbook.add_worksheet('SQL')
+			
+			merge_format = workbook.add_format({
+				'bold':     False,
+				'border':   6,
+				'align':    'left',
+				'valign':   'vcenter',
+				'fg_color': '#FAFAD7',
+				'text_wrap': True
+			})
+			sql_sheet.merge_range('A1:I70', f'{stmt_report}', merge_format)
+
+			worksheet.activate()
+			format_worksheet(worksheet=worksheet, common_format=title_format)
+
+			worksheet.write(0, 0, report_name, title_name_report)
+			worksheet.write(1, 0, f'За квартал: {get_quarter_number(date_first)} ({date_first})', title_name_report)
+
+			row_cnt = 1
+			shift_row = 3
+			cnt_part = 0
+
+			log.info(f'CREATING REPORT {report_code} ...')
+			try:
+				cursor.execute(stmt_report,  first_date=date_first)
+			except oracledb.DatabaseError as e:
+				error, = e.args
+				log.error(f"ERROR. REPORT {report_code}. error_code: {error.code}, error: {error.message}\n{stmt_report}")
+				set_status_report(file_name, 3)
+				return
+			finally:
+				log.info(f'REPORT: {report_code}. Выборка из курсора завершена')
+
+			log.info(f'REPORT: {report_code}. Формируем выходную EXCEL таблицу')
+
+			records = []
+			records = cursor.fetchall()
+			#for record in records:
+			for record in records:
+				col = 1
+				worksheet.write(row_cnt+shift_row, 0, row_cnt, digital_format)
+				for list_val in record:
+					# if col in (1,4):
+					# 	worksheet.write(row_cnt+shift_row, col, list_val, region_name_format)
+					if col in (1,3,5,7,8,9):
+						worksheet.write(row_cnt+shift_row, col, list_val, digital_format)
+					if col in (2,4,6):
+						worksheet.write(row_cnt+shift_row, col, list_val, name_format)
+					col += 1
+				row_cnt += 1
+				cnt_part += 1
+				if cnt_part > 9999:
+					log.info(f'{file_name}. LOADED {row_cnt} records.')
+					cnt_part = 0
+
+			# Шифр отчета
+			worksheet.write(0, 11, report_code, title_name_report)
+			now = datetime.datetime.now()
+			stop_time = now.strftime("%H:%M:%S")
+
+			worksheet.write(1, 6, f'Дата формирования: {now.strftime("%d.%m.%Y ")}({start_time} - {stop_time})', date_format_italic)
+
+			workbook.close()
+			set_status_report(file_name, 2)
+			log.info(f'REPORT: {report_code}. Формирование отчета {file_name} завершено: {now}')
+
+
+def thread_report(file_name: str, date_first: str):
+	import threading
+	log.info(f'THREAD REPORT. {datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")} -> {file_name}')
+	log.info(f'THREAD REPORT. PARAMS NONE')
+	threading.Thread(target=do_report, args=(file_name, date_first), daemon=True).start()
+	return {"status": 1, "file_path": file_name}
+
+
+def get_file_full_name(part_name, params):
+	if 'date_first' in params:
+		trunc_date = datetime.datetime.strptime(params['date_first'], '%Y-%m-%d').replace(day=1)
+		str_trunc_date = datetime.datetime.strftime(trunc_date, '%Y-%m-%d')
+		return f'{part_name}.{str_trunc_date}.xlsx'
+
+
+if __name__ == "__main__":
+    log.info(f'Отчет {report_code} запускается.')
+    do_report('minCO_03.xlsx', '01.10.2022')
